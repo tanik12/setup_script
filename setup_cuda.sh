884b85f0b98ed280f9c8925e53a3b2b8371f9ac2
tanik12 <koyu.tani01@g.softbank.co.jp>
2019-11-05 17:12:59 +0900

    [add] CUDAをinstallするまでのsetup scriptを追加。

---
+++ setup_cuda.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub -y
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1604_10.0.130-1_amd64.deb -y
+sudo apt update -y
+sudo apt install cuda cuda-drivers -y
+sudo reboot
+
+rm cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
+export PATH="/usr/local/cuda/bin:$PATH"
+export LD_LIBRARY_PATH="/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
