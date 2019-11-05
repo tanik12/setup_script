d88d75b983e30243972c3fdafee379e66c1727da
tanik12 <koyu.tani01@g.softbank.co.jp>
2019-11-05 17:31:14 +0900

    "[update]cuda10-0がinstallされるように変更"
    前回のコミットだとCUDA10-1がinstallされるのでnvidiaのレコメンドオプションのversionをinstallしないようにした。
    今回のコミットでCUDA10-0がインストールされるがnvidia-smiを実行すると10-1のままになって謎である。
    nvcc -Vを実行すると、CUDA10-0が表示される。nvidia-smiの表示が10-0にならないか方法を探している。
    とりあえず、今回のコミットを実行するとCUDA10-0がインストールされていることは間違いない。

--- setup_cuda.sh
+++ setup_cuda.sh
@@ -4,7 +4,7 @@ sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1604_10.0.130-1_amd64.deb -y
 sudo apt update -y
-sudo apt install cuda cuda-drivers -y
+sudo apt install -y --no-install-recommends cuda-10-0
 sudo reboot
 
 rm cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
