1e8f1f321b91a9b99ad7dedbb5e60d446121ff01
tanik12 <koyu.tani01@g.softbank.co.jp>
2020-01-05 16:55:20 +0900

    [fix]Tensorflowがインストールできるように修正。

--- setup_tensorflow.sh
+++ setup_tensorflow.sh
@@ -4,8 +4,8 @@ git clone https://github.com/tensorflow/tensorflow.git
 cd tensorflow
 git checkout r1.15
 cd tensorflow/tools/pip_package
-sudo pip install mock absl-py==0.7.0 astor==0.6.0 gast==0.2.2 google_pasta==0.1.6 keras_applications==1.0.8 keras_preprocessing==1.0.5 opt_einsum==2.3.2 enum34==1.1.6 tensorflow-estimator==1.15.1 backports.weakref==1.0rc1
+pip install mock absl-py==0.7.0 astor==0.6.0 gast==0.2.2 google_pasta==0.1.6 keras_applications==1.0.8 keras_preprocessing==1.0.5 opt_einsum==2.3.2 enum34==1.1.6 tensorflow-estimator==1.15.1 backports.weakref==1.0rc1
 sudo python setup.py install
-pip install --user --upgrade tensorflow-gpu==1.15
-
-
+sudo pip install --user --upgrade tensorflow==1.15
+#python3.7でTensorflowをインストールするとうまく動作しないので、python3.6が推奨。
+#1.14まではCPU版とGPU版で分かれていたが、1.15はCPU版とGPU版共にpip install --user --upgrade tensorflowでinstallすること。
