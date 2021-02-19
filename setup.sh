6a97778da913caaaa8ab9b1e4ae5fb67907fcfba
tanik12 <koyu.tani01@g.softbank.co.jp>
2021-02-19 10:10:33 +0900

    [update]セットアップスクリプトの更新

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo apt-get upgrade -y
 sudo apt upgrade -y
 
 #日本語→英語に変換
-#LANG=C xdg-user-dirs-gtk-update
+LANG=C xdg-user-dirs-gtk-update --force
 
 #Mozcのインストール
 sudo apt-get install ibus-mozc -y
@@ -16,5 +16,5 @@ sudo apt install git -y
 sudo apt install terminator -y
 sudo add-apt-repository ppa:graphics-drivers/ppa -y
 sudo apt update
-sudo apt install -y nvidia-410 --no-install-recommends -y
+sudo apt install -y --no-install-recommends nvidia-driver-460
 sudo reboot
