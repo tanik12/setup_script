e150cb21d74717416d345dffc2c95e00e3a03e74
tanik12 <koyu.tani01@g.softbank.co.jp>
2021-02-19 18:58:59 +0900

    [update]installするものを追加

--- setup_ubuntu1804/setup_nvidia_driver.sh
+++ setup_ubuntu1804/setup_nvidia_driver.sh
@@ -11,14 +11,11 @@ sudo apt-get install ibus-mozc -y
 killall ibus-daemon
 ibus-daemon -d -x &
 
-sudo apt install vim -y
-
-sudo apt install git -y
+sudo apt install vim terminator git curl -y
 
 git config --global user.email ""
 git config --global user.name ""
 
-sudo apt install terminator -y
 sudo add-apt-repository ppa:graphics-drivers/ppa -y
 sudo apt update
 sudo apt install -y --no-install-recommends nvidia-driver-460
