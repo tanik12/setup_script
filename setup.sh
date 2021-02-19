a765642c6392d183fb9c2e6837fa089f194195e2
tanik12 <koyu.tani01@g.softbank.co.jp>
2021-02-19 10:20:58 +0900

    [update]update

--- setup.sh
+++ setup.sh
@@ -16,5 +16,6 @@ sudo apt install git -y
 sudo apt install terminator -y
 sudo add-apt-repository ppa:graphics-drivers/ppa -y
 sudo apt update
-sudo apt install -y --no-install-recommends nvidia-driver-460
+#sudo apt install -y --no-install-recommends nvidia-driver-460
+sudo apt install -y --no-install-recommends nvidia-driver-410
 sudo reboot
