211500451a8951150d49854ca4265610a3acdd82
tanik12 <koyu.tani01@g.softbank.co.jp>
2021-02-20 13:41:43 +0900

    [update]docker-composeをinstallできるようにした

--- setup_ubuntu1804/setup_docker.sh
+++ setup_ubuntu1804/setup_docker.sh
@@ -12,3 +12,10 @@ sudo systemctl restart docker
 sudo gpasswd -a $(whoami) docker
 sudo chgrp docker /var/run/docker.sock
 sudo service docker restart
+
+#docker-compose install. 下記から最新のバージョンを確認すること。
+#https://github.com/docker/compose/releases
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo reboot
